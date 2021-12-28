70da6662528f261e7ddb348cab5995c39171067f
Leopold80 <1261763982@qq.com>
2021-12-28 19:14:01 +0800

    add

--- detection/detection_base.py
+++ detection/detection_base.py
@@ -15,15 +15,21 @@ class DetectionBase(mp.Process):
         ie = IECore()
         net = ie.read_network(model=model_xml, weights=model_bin)
 
-        input_blob = next(iter(net.input_info))
-        out_blob = next(iter(net.outputs))
+        self.input_blob = next(iter(net.input_info))
+        self.out_blob = next(iter(net.outputs))
 
-        net.input_info[input_blob].precision = 'FP32'
-        net.outputs[out_blob].precision = 'FP16'
+        net.input_info[self.input_blob].precision = 'FP32'
+        net.outputs[self.out_blob].precision = 'FP16'
 
         self.exec_net = ie.load_network(network=net, device_name=dev)
 
+    def _preprocess(self, *args, **kwargs):
+        raise NotImplementedError()
+
     def _infrence(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def _postprocess(self, *args, **kwargs):
         raise NotImplementedError()
 
     def run(self, *args, **kwargs):
