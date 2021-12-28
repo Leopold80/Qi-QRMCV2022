0b68e344d0a6e4f5b56d68c7ee28cada85e737ec
Leopold80 <1261763982@qq.com>
2021-12-28 16:51:01 +0800

    add

--- IPC/__init__.py
+++ IPC/__init__.py
@@ -1,5 +1,6 @@
-# 尝试一下共享内存的通信方式？ 参考：https://docs.python.org/zh-cn/3/library/multiprocessing.shared_memory.html
+
 from .ipc_abstract import IPCAbstract
 from .rtqueue import Queue
+from .shared_mem import SHM
 
 IPC = Queue
--- IPC/shared_mem.py
+++ IPC/shared_mem.py
@@ -2,6 +2,14 @@ import multiprocessing as mp
 from .ipc_abstract import IPCAbstract
 
 
+"""
+参考资料
+https://docs.python.org/zh-cn/3/library/multiprocessing.shared_memory.html
+
+python3.7尚不支持SharedMemory 先搁起来吧 以后把python升级到3.8就可以了
+"""
+
+
 class SHM(IPCAbstract):
     def __init__(self, ipc):
         super().__init__(ipc)
