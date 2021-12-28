b2bdf37fe1f7bf2016da25935b816698f899638f
Leopold80 <1261763982@qq.com>
2021-12-28 19:22:40 +0800

    add

--- detection/detection_base.py
+++ detection/detection_base.py
@@ -10,31 +10,42 @@ import IPC
 class DetectionBase(mp.Process):
     def __init__(self, model_bin, model_xml, dev, ipc: IPC.IPCAbstract):
         super(DetectionBase, self).__init__()
-
+        # 模型权重文件路径
         model_bin = Path(model_bin)
+        # 模型结构文件路径
         model_xml = Path(model_xml)
 
+        # 多进程通信模块
         self._ipc = ipc
 
+        # 加载推理引擎
         ie = IECore()
+        # 读取网络
         net = ie.read_network(model=model_xml, weights=model_bin)
 
+        # 输入输出信息
         self.input_blob = next(iter(net.input_info))
         self.out_blob = next(iter(net.outputs))
 
+        # 设置输入输出精度
         net.input_info[self.input_blob].precision = 'FP32'
         net.outputs[self.out_blob].precision = 'FP16'
 
+        # 加载可执行网络
         self.exec_net = ie.load_network(network=net, device_name=dev)
 
+    # 图像预处理
     def _preprocess(self, *args, **kwargs):
         return self._ipc.pull()
 
+    # 推理
     def _infrence(self, *args, **kwargs):
         raise NotImplementedError()
 
+    # 推理结果后处理
     def _postprocess(self, *args, **kwargs):
         raise NotImplementedError()
 
+    # 线程运行核心函数
     def run(self, *args, **kwargs):
         raise NotImplementedError()
