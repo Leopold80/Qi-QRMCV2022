35fe66d759cb3dcb2e6a2be72c7d4a8a1e94ccb1
Leopold80 <1261763982@qq.com>
2021-12-28 19:19:19 +0800

    add

--- detection/detection_base.py
+++ detection/detection_base.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 import multiprocessing as mp
 from openvino.inference_engine import IECore
+import IPC
 
 
+# 消费者进程基类，继承mp.Process实现多进程及跨进程通信
+# 同时也实现了探测器的功能的函数抽象
+# 现在的问题是串口通讯要不要单独做一个进程？如果不做单独的进程的话看看他的发送是否为阻塞模式。
 class DetectionBase(mp.Process):
-    def __init__(self, model_bin, model_xml, dev, ipc):
+    def __init__(self, model_bin, model_xml, dev, ipc: IPC.IPCAbstract):
         super(DetectionBase, self).__init__()
 
         model_bin = Path(model_bin)
@@ -24,7 +28,7 @@ class DetectionBase(mp.Process):
         self.exec_net = ie.load_network(network=net, device_name=dev)
 
     def _preprocess(self, *args, **kwargs):
-        raise NotImplementedError()
+        return self._ipc.pull()
 
     def _infrence(self, *args, **kwargs):
         raise NotImplementedError()
